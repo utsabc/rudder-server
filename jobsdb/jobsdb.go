ee37a1469976ef6da09c5c8b8c9a45e962810ffd
Chandra shekar Varkala <chandra@rudderlabs.com>
2021-01-27 13:10:31 +0530

    scale up fix in separate gw case

--- jobsdb/jobsdb.go
+++ jobsdb/jobsdb.go
@@ -766,7 +766,7 @@ func (jd *HandleT) getDSRangeList(refreshFromDB bool) []dataSetRangeT {
 		if !minID.Valid || !maxID.Valid {
 			continue
 		}
-		
+
 		if idx < len(dsList)-1 && (jd.inProgressMigrationTargetDS == nil || jd.inProgressMigrationTargetDS.Index != ds.Index) {
 			//TODO: Cleanup - Remove the line below and jd.inProgressMigrationTargetDS
 			jd.assert(minID.Valid && maxID.Valid, fmt.Sprintf("minID.Valid: %v, maxID.Valid: %v. Either of them is false for table: %s", minID.Valid, maxID.Valid, ds.JobTable))
